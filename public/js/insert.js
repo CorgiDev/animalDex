c2a19f7d03a003b7c01280666973c0aabbe066db
Elizabeth Gray <EGrayTech@users.noreply.github.com>
2018-07-14 21:17:22 -0400

    Experimenting with inserting html from a js file.

---
+++ public/js/insert.js
@@ -0,0 +1,14 @@
+//This is all bits of html being inserted into the pages of the site. 
+//The only exception is the form elements and list. They are in app.js.
+
+$('#logo').html('<h1>AnimalDex</h1>' +
+                '<h2>Can you see them all?</h2>');
+
+$('#navigation').html('<ul>' +
+                      '<li>Home</li>' + 
+                      '<li>Contact</li>'+ 
+                      '</ul>');
+
+$('#yourDivName').html('yourtHTML');
+
+$('#yourDivName').html('yourtHTML');
